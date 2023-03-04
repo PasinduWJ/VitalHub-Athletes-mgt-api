15b003db87b90907e17999ac59c597bcfcfde6d7
Pasindu Jayasekara <pasindu_j@epic.lk>
2023-03-04 12:50:53 +0530

    initial commit

---
+++ src/main/java/com/vitalhub/ams/athletesmanagementapi/AthletesManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitalhub.ams.athletesmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthletesManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthletesManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitalhub/ams/athletesmanagementapi/AthletesManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalhub.ams.athletesmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletesManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
